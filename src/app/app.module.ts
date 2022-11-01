b12f23cde6b8314d311154d3d63cd63633c1ae7c
chris kim <chris@inkubasia.com>
2022-11-01 17:48:50 +0600

    lazily loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { NativeScriptFormsModule } from '@nativescript/angular'
 import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular'
 
 import { AppComponent } from './app.component'
-import { CurrentChallengeComponent } from './challenges/current-challenge/current-challenge.component'
 import { StackComponent } from './layouts/stack/stack.component'
-import { ChallengeEditComponent } from './challenges/challenge-edit/challenge-edit.component'
 import { AuthComponent } from './auth/auth.component'
-import { TodayComponent } from './challenges/today/today.component'
 import { AppRoutingModule } from './app-routing.module'
-import { ActionBarComponent } from './shared/ui/action-bar/action-bar.component'
-import { ChallengeTabsComponent } from './challenges/challenge-tabs/challenge-tabs.component'
 import { DayModalComponent } from './challenges/day-modal/day-modal.component'
+import { SharedModule } from '../app/shared/shared.module'
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -21,16 +17,12 @@ import { DayModalComponent } from './challenges/day-modal/day-modal.component'
     NativeScriptFormsModule,
     AppRoutingModule,
     NativeScriptUISideDrawerModule,
+    SharedModule,
   ],
   declarations: [
     AppComponent,
-    CurrentChallengeComponent,
     StackComponent,
-    ChallengeEditComponent,
     AuthComponent,
-    TodayComponent,
-    ActionBarComponent,
-    ChallengeTabsComponent,
     DayModalComponent,
   ],
   providers: [],
